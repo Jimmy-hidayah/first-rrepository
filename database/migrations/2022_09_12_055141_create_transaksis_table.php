33506861bf7bc17e835eb7ba966206a095fd82c4
jim <jimjimdes@gmail.com>
2022-09-16 14:33:15 +0700

    wip

---
+++ database/migrations/2022_09_12_055141_create_transaksis_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTransaksisTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('transaksis', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained('users')->cascadeOnDelete();
+            $table->foreignId('produk_id')->constrained('produks')->cascadeOnDelete();
+            $table->string('nama_produk');
+            $table->string('harga_produk');
+            $table->integer('jumlah_produk');
+            $table->date('tanggal');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('transaksis');
+    }
+}
